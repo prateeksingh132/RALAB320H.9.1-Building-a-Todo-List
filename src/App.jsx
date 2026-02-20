baa828369d0961acd154f769dbf2ff2a2a10f25d
prateeksingh132 <kumarpks132@gmail.com>
2026-02-20 10:58:15 -0700

    working on fetch api data functionality now. added the load data button with handler function to app.jsx, tested, works well so far. will work on fetch logic inside the function now…

--- src/App.jsx
+++ src/App.jsx
@@ -23,13 +23,33 @@ function App() {
   const [todos, dispatch] = useReducer(todoReducer, initialState);
 
 
+  // handler function where i wil put all my logic for fetching the api data
+  const handleFetch = () => {
+
+
+    ////////////TESTING
+    console.log("TESTING: fetch button clicked!!!!!!");
+    ////////////
+
+  };
+
 
   return (
     <div className="app-container">
       <h1>Todo List</h1>
 
       {/* adding the new component todoform here at the start for adding task functionality  */}
-      <TodoForm dispatch={dispatch}/>
+      <TodoForm dispatch={dispatch} />
+
+      {/* i am adding a button below the form which will fetch data from the external api */}
+      <button
+        onClick={handleFetch}
+        // FUTUREWORK: add it later in the app.css
+        style={{ marginBottom: '20px', padding: '10px 15px', background: '#007bff', color: 'white', border: 'none', borderRadius: '4px', cursor: 'pointer' }}
+      >
+        Load API Data
+      </button>
+
 
       <div>
         {/* i have created a new todoitems componenet, replacing the divs with my todoitem componenent now */}
