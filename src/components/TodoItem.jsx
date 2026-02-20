a1b0d92c50f0637286b94aafd3cfe8aa6b2452eb
prateeksingh132 <kumarpks132@gmail.com>
2026-02-20 02:07:22 -0700

    added the save button functionality. tested it in the browser, works well so far...

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -6,18 +6,35 @@ export default function TodoItem({ todo, dispatch }) {
 
     // goal: i am adding local state to handle swapping between read only mode and edit mode.
 
-    /////logic for edit mode
+    /////logic for edit functionality
     // i need a way to change the text into an editable input box. in superhero exercise we did in class (charcard.jsx), we did something similar, 
     // that the easiest way to swap a display for an edit form is just using a local boolean toggle with usestate. 
     // the idea is that this edit mode doesnt need to be in the global reducer because its just a temporary ui state for this specific component. 
     // so i added isEditing to track true/false and editTitle to track what i actually type into the box. 
     // then i just wrap the inside of my div in a big ternary operator: if isEditing is true render the input box. if false render what we already have
 
-    // using local state just for the ui toggle, exactly like the CharCard warmup.
+    // using local state just for the ui toggle, exactly like the superhero excercise
     const [isEditing, setIsEditing] = useState(false);
 
     // keeping track of what the user is typing in the edit box. it will default to the current title.
     const [editTitle, setEditTitle] = useState(todo.title);
+
+
+    /////// logic for save functionality
+    //// similar to superhero excercise
+    // goal: i wanna handle the save button click to update the state and close the edit box
+    const handleSave = () => {
+        // logic: dispatch the edit_task action. 
+        // IMPORTANT: in the reducer payload needs to have both the id and the title
+        dispatch({
+            type: 'EDIT_TASK',
+            payload: { id: todo.id, newTitle: editTitle }
+        });
+
+        // after sending the update to the global state, changing the local state back to false to hide the input box
+        setIsEditing(false);
+    };
+
 
     return (
         // logic: i m just gonna build a basic ui right now with basic styling, just to get a visual layout. i will wire up the dispatch actions in the next steps.
@@ -34,7 +51,7 @@ export default function TodoItem({ todo, dispatch }) {
                     />
                     {/* save button just console logs for right now, will add logic later */}
                     {/* FUTUREWORK: */}
-                    <button onClick={() => console.log("save clicked!")}>Save</button>
+                    <button onClick={handleSave}>Save</button>
 
                     <button onClick={() => setIsEditing(false)}>Cancel</button>
                 </>
