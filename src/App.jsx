4fd0154bd174f0a7d68006ba99b63e8a081f091c
prateeksingh132 <kumarpks132@gmail.com>
2026-02-20 00:22:20 -0700

    initialized usereducer in app.jsx, temporarily mapped out the titles into simple divs just to verify the state connects to the ui properly. tested in the browser and i can see the 5 initial tasks. will work on the actual todoitem component to replace these temporary divs now…

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,39 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import todoReducer, { initialState } from './reducers/todoReducer';
+import { useReducer } from 'react';
 
-////////////TESTING
-const testAction = { type: 'EDIT_TASK', payload: { id: 4, newTitle: "EDITEDDDDDD..." } };
-const testState = todoReducer(initialState, testAction);
-
-////////////TESTING
-console.log("TESTING add task: ", testState);
-////////////
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+
+
+  // ////////////TESTING
+  // const testAction = { type: 'EDIT_TASK', payload: { id: 4, newTitle: "EDITEDDDDDD..." } };
+  // const testState = todoReducer(initialState, testAction);
+
+  ////////////TESTING
+  //console.log("TESTING add task: ", testState);
+  ////////////
+
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
+
+
 
   return (
-    <>
+    <div className="app-container">
+      <h1>Todo List</h1>
 
-      <div className="app-container">
-        <h1>Todo List</h1>
+      {/* for the time being, i am gonna use the .map() to loop through my state and put the titles to the screen for now. */}
+      <div>
+        {todos.map(todo => (
+          <div key={todo.id} style={{ border: '1px solid white', background: 'yellow' }}>
+            {todo.title}
+          </div>
+        ))}
       </div>
-
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
